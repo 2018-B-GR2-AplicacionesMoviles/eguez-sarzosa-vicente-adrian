6c882f3c12ec497387e779b895c204793d629e7e
Adrian Eguez <eadepto@hotmail.com>
2018-11-08 15:55:23 -0500

    layouts

--- 02-Android/2018MovilesGR2/app/src/main/java/com/example/usrdel/a2018movilesgr2/MainActivity.kt
+++ 02-Android/2018MovilesGR2/app/src/main/java/com/example/usrdel/a2018movilesgr2/MainActivity.kt
@@ -1,12 +1,34 @@
 package com.example.usrdel.a2018movilesgr2
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        boton_navegar
+                .setOnClickListener {
+                    /*
+                    Log.i("navegacion","Hola")
+                    Log.w("navegacion","Hola")
+                    Log.d("navegacion","Hola")
+                    Log.e("navegacion","Hola")
+                    Log.v("navegacion","Hola")
+                    */
+                    this.irAPantallaDeBotones()
+                }
     }
+
+    fun irAPantallaDeBotones() {
+        // INTENT
+        val intentIrABotones = Intent(this, ButtonActivity::class.java)
+        this.startActivity(intentIrABotones)
+    }
+
 }
