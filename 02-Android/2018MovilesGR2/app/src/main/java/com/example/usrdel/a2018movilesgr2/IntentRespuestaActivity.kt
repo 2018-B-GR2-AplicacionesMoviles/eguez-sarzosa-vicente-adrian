e6b765a332768293395c93b706867c066d778f33
Adrian Eguez <eadepto@hotmail.com>
2018-11-15 15:08:08 -0500

    intent resultado de contacots

--- 02-Android/2018MovilesGR2/app/src/main/java/com/example/usrdel/a2018movilesgr2/IntentRespuestaActivity.kt
+++ 02-Android/2018MovilesGR2/app/src/main/java/com/example/usrdel/a2018movilesgr2/IntentRespuestaActivity.kt
@@ -1,6 +1,8 @@
 package com.example.usrdel.a2018movilesgr2
 
+import android.app.Activity
 import android.content.Intent
+import android.database.Cursor
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.ContactsContract
@@ -21,15 +23,59 @@ class IntentRespuestaActivity : AppCompatActivity() {
 
     fun enviarIntentRespuesta() {
         val intentRespuesta = Intent(Intent.ACTION_PICK, ContactsContract.CommonDataKinds.Phone.CONTENT_URI)
-        this.startActivityForResult(intentRespuesta, 101);
+        this.startActivityForResult(intentRespuesta, requestCodeContactos);
 
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-        Log.i("intent-respuesta", "requestCode: ${requestCode}")
-        Log.i("intent-respuesta", "resultCode: ${resultCode}")
-        Log.i("intent-respuesta", "data: ${data}")
+
+        when (requestCode) {
+            requestCodeContactos -> {
+
+                when (resultCode) {
+                    RESULT_OK -> {
+                        Log.i("contactos", "respuesta ${data}")
+                        var cursor: Cursor? = null
+
+                        try {
+
+                            var numeroTelefono: String? = null
+                            var nombre: String? = null
+                            val uri = data?.data
+                            cursor = contentResolver.query(uri, null, null, null, null)
+
+                            cursor!!.moveToFirst()
+
+                            val indiceTelefono = cursor!!.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER)
+                            val indiceNombre = cursor!!.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME)
+
+                            numeroTelefono = cursor!!.getString(indiceTelefono)
+                            nombre = cursor!!.getString(indiceNombre)
+
+                            Log.i("contactos", "Telefono: ${numeroTelefono} - Nombre: ${nombre}")
+
+                        } catch (e: Exception) {
+                            Log.i("contactos", "Error")
+                        }
+                    }
+                    RESULT_CANCELED -> {
+                        Log.i("contactos", "No se selecciono ningun contacto")
+                    }
+                }
+
+
+            }
+            else -> {
+                Log.e("indefinido", "indefinido")
+            }
+        }
+
+
+    }
+
+    companion object {
+        val requestCodeContactos = 101
     }
 }
 
