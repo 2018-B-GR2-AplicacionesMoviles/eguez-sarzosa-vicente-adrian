b5b2d0b278282536efabd447fd3c4726bd0ba724
Adrian Eguez <eadepto@hotmail.com>
2019-01-17 16:46:22 -0500

    actualizacion

--- 02-Android/2018MovilesGR2/app/src/main/java/com/example/usrdel/a2018movilesgr2/HttpActivity.kt
+++ 02-Android/2018MovilesGR2/app/src/main/java/com/example/usrdel/a2018movilesgr2/HttpActivity.kt
@@ -3,8 +3,11 @@ package com.example.usrdel.a2018movilesgr2
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import com.beust.klaxon.Klaxon
 import com.github.kittinunf.fuel.httpGet
+import com.github.kittinunf.fuel.httpPost
 import com.github.kittinunf.result.Result
+import java.util.*
 
 class HttpActivity : AppCompatActivity() {
 
@@ -12,22 +15,37 @@ class HttpActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_http)
 
-        val url = "http://172.31.105.224:1337/Estudianteasdfaasdfasd"
+        val url = "http://172.31.105.224:1337/Estudiante"
+
+        val adrian = UsuarioHttpCreateDto("Vicente", "Eguez")
+        val adrianJson = Klaxon().toJsonString(adrian)
+        val parametros = listOf(
+                "nombre" to "Adrian",
+                "apellido" to "Eguez"
+        )
+
 
 
         url
-                .httpGet()
+                .httpPost(parametros)
                 .responseString { request, response, result ->
 
                     when (result) {
                         is Result.Failure -> {
                             val exepcion = result.getException()
                             Log.i("http", "Error: ${exepcion}")
+                            Log.i("http", "Error: ${response}")
 
                         }
                         is Result.Success -> {
-                            val data = result.get()
-                            Log.i("http", "Datos: ${data}")
+
+                            val usuarioString = result.get()
+
+                            val usuarioClase: UsuarioHttp? = Klaxon()
+                                    .parse<UsuarioHttp>(usuarioString)
+
+                            Log.i("http", "Datos: ${usuarioClase?.nombre}")
+
                         }
                     }
                 }
@@ -35,3 +53,12 @@ class HttpActivity : AppCompatActivity() {
 
     }
 }
+
+class UsuarioHttp(var nombre: String,
+                  var apellido: String,
+                  var createdAt: Long? = null,
+                  var updatedAt: Long? = null,
+                  var id: Int? = null) {}
+
+class UsuarioHttpCreateDto(var nombre: String,
+                           var apellido: String) {}
