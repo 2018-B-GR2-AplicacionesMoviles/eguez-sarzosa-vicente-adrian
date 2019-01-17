0a6b667b1e7101f43b400052391ab4294f9ab023
Adrian Eguez <eadepto@hotmail.com>
2019-01-17 14:45:23 -0500

    backend

--- 02-Android/2018MovilesGR2/app/src/main/java/com/example/usrdel/a2018movilesgr2/Usuario.kt
+++ 02-Android/2018MovilesGR2/app/src/main/java/com/example/usrdel/a2018movilesgr2/Usuario.kt
@@ -43,3 +43,98 @@ class Usuario(public var nombre: String,
 
 
 // val adrian = Usuario("asdasd")
+
+
+// protocolo ip puerto link
+// http://127.0.0.1:1337/inicio
+// Query Params -> Consulta
+// http://127.0.0.1:1337/inicio?nombre=Adrian&cedula=1718137159
+// Body/Form
+// http://127.0.0.1:1337/inicio
+// <form></form> / HTTP ->
+// Ruta
+// http://127.0.0.1:1337/cuenta/200810275/facultad/sistemas
+// http://127.0.0.1:1337/cuenta/201012431/facultad/matematica
+// http://127.0.0.1:1337/cuenta/201112341/facultad/fisica
+// http://127.0.0.1:1337/cuenta/201112341/facultad/electrica
+
+// http://127.0.0.1:1337/cuenta GET
+// http://127.0.0.1:1337/cuenta POST
+// http://127.0.0.1:1337/cuenta PUT
+// http://127.0.0.1:1337/cuenta DELETE
+
+// http://127.0.0.1:1337/usuario/crearUsuario POST
+// http://127.0.0.1:1337/usuario/crear POST
+// http://127.0.0.1:1337/usuario/creando POST
+// http://127.0.0.1:1337/usuario/nuevo POST
+
+// REST / RESTFUL
+
+
+// http://127.0.0.1:1337/
+
+
+// MODELO -> Tabla de la base
+
+// http://127.0.0.1:1337/Estudiante
+// Estudiante
+
+// Obtener Todos -> Todos Estudiantes
+// http://127.0.0.1:1337/Estudiante GET
+
+// Crear -> Nuevo Estudiante
+// http://127.0.0.1:1337/Estudiante POST
+// BodyParams FormParams (Parametros)
+
+// BuscarPorId -> 1 estudiante
+// http://127.0.0.1:1337/Estudiante/1 GET
+
+
+// BorrarPorId -> ¿?
+// http://127.0.0.1:1337/Estudiante/1 DELETE
+
+// -> BACKEND
+
+// ActualizarPorId -> 1 estudiante
+// http://127.0.0.1:1337/Estudiante/1 PUT
+// BodyParams FormParams (Parametros)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
