05d6a1b974d8b1e42078459e7db9a7630f0891e3
Adrian Eguez <eadepto@hotmail.com>
2019-01-24 15:50:53 -0500

    list view

--- 02-Android/2018MovilesGR2/app/src/main/java/com/example/usrdel/a2018movilesgr2/Menu2Activity.kt
+++ 02-Android/2018MovilesGR2/app/src/main/java/com/example/usrdel/a2018movilesgr2/Menu2Activity.kt
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity
 import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.ArrayAdapter
 import com.example.usrdel.a2018movilesgr2.R.styleable.Snackbar
 
 
@@ -15,16 +16,28 @@ class Menu2Activity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu2)
-
-
         setSupportActionBar(toolbar)
-
-
-
+        llenarListView()
         fab.setOnClickListener { view ->
 
         }
     }
+
+    fun llenarListView() {
+        val numeros = arrayListOf("1", "2", "3", "4", "5", "6", "7", "8", "9", "10")
+
+        val adapter = ArrayAdapter<String>(
+                this,
+                android.R.layout.simple_list_item_1, numeros
+        )
+
+        list_view_context_menu.adapter = adapter
+
+        // Registrar para que sirvan los menus contextuales
+
+        registerForContextMenu(list_view_context_menu)
+    }
+
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         val inflater = menuInflater
