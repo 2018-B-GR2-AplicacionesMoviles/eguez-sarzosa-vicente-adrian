8059c6f46da5532cee8ef69ba28d1682b1b9cbd7
Adrian Eguez <eadepto@hotmail.com>
2019-02-01 15:17:37 -0500

    funcion

--- 02-Android/2018MovilesGR2/app/src/main/java/com/example/usrdel/a2018movilesgr2/MapaContenedorActivity.kt
+++ 02-Android/2018MovilesGR2/app/src/main/java/com/example/usrdel/a2018movilesgr2/MapaContenedorActivity.kt
@@ -1,12 +1,89 @@
 package com.example.usrdel.a2018movilesgr2
 
+import android.content.pm.PackageManager
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.app.ActivityCompat
+import android.support.v4.content.ContextCompat
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.model.Polygon
+import com.google.android.gms.maps.model.Polyline
 
-class MapaContenedorActivity : AppCompatActivity() {
+class MapaContenedorActivity :
+        AppCompatActivity(),
+        OnMapReadyCallback,
+        GoogleMap.OnCameraMoveStartedListener,
+        GoogleMap.OnCameraMoveListener,
+        GoogleMap.OnCameraMoveCanceledListener,
+        GoogleMap.OnCameraIdleListener,
+        GoogleMap.OnPolylineClickListener,
+        GoogleMap.OnPolygonClickListener {
+
+    var usuarioTienePermisosLocalizacion = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_mapa_contenedor)
+
+        solicitarPermisosLocalizacion()
+
+
+    }
+
+
+    fun solicitarPermisosLocalizacion() {
+
+        val tienePermisos = ContextCompat
+                .checkSelfPermission(
+                        this.applicationContext,
+                        android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
+
+        if (tienePermisos) {
+
+            usuarioTienePermisosLocalizacion = true
+
+        } else {
+
+            // Solicitar permisos
+
+            ActivityCompat.requestPermissions(
+                    this,
+                    arrayOf(
+                            android.Manifest.permission.ACCESS_FINE_LOCATION
+                    ),
+                    1
+            )
+
+        }
+    }
+
+
+    override fun onMapReady(p0: GoogleMap?) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onCameraMoveStarted(p0: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onCameraMove() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onCameraMoveCanceled() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onCameraIdle() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onPolylineClick(p0: Polyline?) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onPolygonClick(p0: Polygon?) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 }
